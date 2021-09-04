0f2e1e71f46bab8f1682173f4e7e592ab7eaeb3a
Badredine Benfraj <badrbenfraj@mail.com>
2021-09-04 11:45:05 +0100

    feat(): add Gov description

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 $description = $(".description");
+$map_description = $(".map__description");
 
   $('.land').hover(function() {
     
     $(this).attr("class", "land");
     $description.addClass('active');
     $description.html($(this).attr('title'));
+    $map_description.html($(this).attr('description'));
   }, function() {
     $description.removeClass('active');
   });
